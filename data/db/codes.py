0a55ea83460785983394dc252293ab50045714c9
Valor760 <valorcool@gmail.com>
2021-02-12 20:33:05 +0200

    gitignore db

--- data/db/codes.py
+++ data/db/codes.py
@@ -1,6 +1,6 @@
 TMI_TOKEN = "oauth:lwypo8gbbxdj20vdesfivtpgl740rs"
 CLIENT_ID = "1gkf7h1glsxebahdji24zy53r423vm"
-BOT_NICK = "6u6YT1K"
+BOT_NICK = "BOT_EHOTUK"
 BOT_PREFIX = "!"
 CHANNEL = ["enoootuuuk", "Valor760"]
 # CHANNEL = ["Valor760"]
