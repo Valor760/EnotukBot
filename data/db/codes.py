dbdd0e7fa0c2d49aea201c5ec9a7a53e0f4ef0be
Valor760 <31585840+Valor760@users.noreply.github.com>
2021-02-15 10:25:48 +0200

    Delete codes.py

--- data/db/codes.py
+++
@@ -1,6 +0,0 @@
-TMI_TOKEN = "oauth:lwypo8gbbxdj20vdesfivtpgl740rs"
-CLIENT_ID = "1gkf7h1glsxebahdji24zy53r423vm"
-BOT_NICK = "BOT_EHOTUK"
-BOT_PREFIX = "!"
-# CHANNEL = ["enoootuuuk", "Valor760"]
-CHANNEL = ["Valor760"]
