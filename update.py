34432317b683f19625d3338b83204bddfb7798a3
valor <valorcool@gmail.com>
2021-08-17 22:09:20 +0300

    version for desktop

--- update.py
+++ update.py
@@ -7,19 +7,20 @@ def y_n(q):
         elif ri.lower() == 'n': return False
 
 def main():
-    update = y_n("Update the bot?")
-    if update:
-        try:
-            subprocess.check_call('git --version', shell=True, stdout=subprocess.DEVNULL)
-        except subprocess.CalledProcessError:
-            raise EnvironmentError("No Git installed!")
+    try:
+        subprocess.check_call('git --version', shell=True, stdout=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        raise EnvironmentError("No Git installed!")
 
-        print('Passed Git check.....')
+    print('Passed Git check.....')
 
-        sp = subprocess.check_output('git status --porcelain', shell=True, universal_newlines=True)
-        if sp:
-            # oshit = y_n('You have modified files that are tracked by Git (e.g the bot\'s source files).\n'
-            #             'Should we try resetting the repo? You will lose local modifications.')
+    sp = subprocess.check_output('git status --porcelain', shell=True, universal_newlines=True)
+    if sp:
+        # oshit = y_n('You have modified files that are tracked by Git (e.g the bot\'s source files).\n'
+        #             'Should we try resetting the repo? You will lose local modifications.')
+        update = y_n("New bot version found!\n"
+                     "Update the bot?")
+        if update:
             if False:
                 try:
                     subprocess.check_call('git reset --hard', shell=True)
@@ -30,8 +31,10 @@ def main():
                 subprocess.check_call('git pull', shell=True)
             except subprocess.CalledProcessError:
                 raise OSError("Could not update the bot!")
-        else:
-            print("No bot updates found!!")
+    else:
+        print("No bot updates found!!")
+    
+    input("Press Enter to continue...")
             
 
 
