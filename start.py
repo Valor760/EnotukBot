aa67e5f159642a884923632bef373b6b62200e89
valor <valorcool@gmail.com>
2021-08-17 21:57:01 +0300

    version for desktop

--- start.py
+++ start.py
@@ -18,40 +18,8 @@ def update_library():
             raise OSError("Could not update libraries.".format(sys.executable))
 
 
-def y_n(q):
-    while True:
-        ri = input(f'{q} (y/n): ')
-        if ri.lower() == 'y': return True
-        elif ri.lower() == 'n': return False
-
-
 def main():
     update_library()
-    if platform.system() == 'Linux':
-        update = y_n("Update the bot?")
-        if update:
-            try:
-                subprocess.check_call('git --version', shell=True, stdout=subprocess.DEVNULL)
-            except subprocess.CalledProcessError:
-                raise EnvironmentError("No Git installed!")
-
-            print('Passed Git check.....')
-
-            sp = subprocess.check_output('git status --porcelain', shell=True, universal_newlines=True)
-            if sp:
-                oshit = y_n('You have modified files that are tracked by Git (e.g the bot\'s source files).\n'
-                            'Should we try resetting the repo? You will lose local modifications.')
-                if oshit:
-                    try:
-                        subprocess.check_call('git reset --hard', shell=True)
-                    except subprocess.CalledProcessError:
-                        raise OSError("Could not reset the directory to a clean state.")
-
-            try:
-                subprocess.check_call('git pull', shell=True)
-            except subprocess.CalledProcessError:
-                raise OSError("Could not update the bot!")
-
     from lib.bot import bot
     bot.run()
 
